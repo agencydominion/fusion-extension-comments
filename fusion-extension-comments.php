593d4dae28a5496dee3149a4153c8849cb98abd4
Jeff Chew <jeff.chew@agencydominion.com>
2016-07-25 12:30:11 -0300

    Version 1.0.0

---
+++ fusion-extension-comments.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @package Fusion_Extension_Comments
+ */
+/**
+ * Plugin Name: Fusion : Extension - Comments
+ * Plugin URI: http://fusion.1867dev.com
+ * Description: Comments Extension Package for Fusion.
+ * Version: 1.0.0
+ * Author: Agency Dominion
+ * Author URI: http://agencydominion.com
+ * License: GPL2
+ */
+ 
+/**
+ * FusionExtensionComments class.
+ *
+ * Class for initializing an instance of the Fusion Comments Extension.
+ *
+ * @since 1.0.0
+ */
+
+
+class FusionExtensionComments	{ 
+	public function __construct() {
+						
+		// Initialize the language files
+		load_plugin_textdomain( 'fusion-extension-comments', false, plugin_dir_url( __FILE__ ) . 'languages' );
+		
+		// Plugin activation / deactivation hooks
+		register_activation_hook( __FILE__, array($this, 'plugin_activated') );
+		register_deactivation_hook( __FILE__, array($this, 'plugin_deactivated') );
+	}
+	
+	/**
+	 * Function to run when plugin is activated.
+	 *
+	 * @since 1.0.0
+	 *
+	 */
+	 
+	public function plugin_activated(){
+		//add htaccess rules to disable direct access to the wp-comments-post.php to reduce comment spam
+		$site_parse_url = parse_url(site_url());
+		$rules = array();
+		$rules[] = 'RewriteEngine On';
+		$rules[] = 'RewriteCond %{REQUEST_METHOD} POST';
+		$rules[] = 'RewriteCond %{REQUEST_URI} .wp-comments-post\.php*';
+		$rules[] = 'RewriteCond %{HTTP_REFERER} !.*'.$site_parse_url['host'].'.* [OR]';
+		$rules[] = 'RewriteCond %{HTTP_USER_AGENT} ^$';
+		$rules[] = 'RewriteRule (.*) http://%{REMOTE_ADDR}/$ [R=301,L]';
+		$home_path = get_home_path();
+		$htaccess_file = $home_path.'.htaccess';
+		if (file_exists($htaccess_file) && is_writable($home_path) &&  is_writable($htaccess_file)) {
+	        insert_with_markers( $htaccess_file, 'Fusion Comment Access', $rules );        
+		}
+    }
+
+
+	/**
+	 * Function to run when plugin is deactivated.
+	 *
+	 * @since 1.0.0
+	 *
+	 */
+	 
+    public function plugin_deactivated(){
+	    //remove rules htaccess rules to disable direct access to the wp-comments-post.php to reduce comment spam
+		$home_path = get_home_path();
+		$htaccess_file = $home_path.'.htaccess';
+		if (file_exists($htaccess_file) && is_writable($home_path) &&  is_writable($htaccess_file)) {
+	    	insert_with_markers( $htaccess_file, 'Fusion Comment Access', '' ); 
+		}
+        
+    }
+}
+
+$fsn_extension_comments = new FusionExtensionComments();
+
+//EXTENSIONS
+
+//comments
+require_once('includes/extensions/comments.php');
+
+?>
---
+++ includes/extensions/comments.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package Fusion_Extension_Comments
+ */
+
+/**
+ * Comments Template Extension.
+ *
+ * Function for adding the WordPress Comments section.
+ *
+ * @since 1.0.0
+ */
+
+// Comments Template
+
+add_action('init', 'fsn_init_comments', 12);
+function fsn_init_comments() {	
+	
+	//OUTPUT SHORTCODE
+	function fsn_comments_shortcode( $atts, $content = null ) {
+		
+		$output = '<div class="fsn-comments '. fsn_style_params_class($atts) .'">';
+			ob_start();
+			comments_template();
+			$output .= ob_get_clean();
+		$output .= '</div>';
+		
+		return $output;
+	}
+	
+	add_shortcode( 'fsn_comments', 'fsn_comments_shortcode' );
+			
+	//MAP SHORTCODE
+	if (function_exists('fsn_map')) {
+		fsn_map(array(
+			'name' => __('Comments', 'fusion-extension-comments'),
+			'shortcode_tag' => 'fsn_comments',
+			'description' => __('Add comments section. Displays a field for users to input comments.', 'fusion-extension-comments'),
+			'icon' => 'comment'
+		));
+	}
+}
+
+?>
