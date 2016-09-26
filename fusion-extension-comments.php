ec554c8e88ea77a1122449a8b95c44eb641073f1
Jeff Chew <jeff.chew@agencydominion.com>
2016-09-26 15:43:07 -0300

    Update version numberUpdate version number

--- fusion-extension-comments.php
+++ fusion-extension-comments.php
@@ -6,7 +6,7 @@
  * Plugin Name: Fusion : Extension - Comments
  * Plugin URI: http://www.agencydominion.com/fusion/
  * Description: Comments Extension Package for Fusion.
- * Version: 1.1.0
+ * Version: 1.1.2
  * Author: Agency Dominion
  * Author URI: http://agencydominion.com
  * License: GPL2
